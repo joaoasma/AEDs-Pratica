cf984db13253f69da525bd6cd914c7e83fde8e5c
João Antônio de Souza Martins <joaoantonio.martins@sou.unifal-mg.edu.br>
2025-07-01 11:34:43 -0300

    Add files via upload

---
+++ Ordenação/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "ordenacao.hpp"
+#include <locale>
+#include <iostream>
+
+int main() {
+    int v[9];
+
+    fazer_vetor(v, 9);
+    printar_vetor(v, 9);
+    
+    return 0;
+}
